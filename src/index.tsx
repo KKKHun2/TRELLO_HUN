d4495e8c3d8dc566b6402cf3f6d744e62fa43a5c
KKKHun2 <1224kim2son@naver.com>
2023-09-19 10:56:07 +0900

    ✨리코일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import {RecoilRoot} from "recoil";
 
 ReactDOM.render(
+  <RecoilRoot>
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </RecoilRoot>,
   document.getElementById("root")
 );
