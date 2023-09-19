3a04243cd74f29dea0e54ef93a3584f14485bf3f
KKKHun2 <1224kim2son@naver.com>
2023-09-19 10:55:35 +0900

    ⚡기능 동작 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import './styles.css';
+import { DragDropContext, Droppable, Draggable,DropResult } from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import { toDoState } from "./atoms";
+
 
 function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos];
+      console.log("Delete item on", source.index);
+      console.log(toDosCopy);
+      toDosCopy.splice(source.index, 1);
+      console.log("Deleted item");
+      console.log(toDosCopy);
+
+      console.log("Put back", draggableId, "on ", destination.index);
+      toDosCopy.splice(destination?.index, 0, draggableId);
+      console.log(toDosCopy);
+      return toDosCopy;
+    });
+  };
   return (
-    <div className="bg-blue-900 p-4 m-4">
-      <h1 className="text-3xl text-white">tailwind test!</h1>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      
+      <div className="flex justify-center items-center h-screen max-w-screen-sm mx-auto bg-blue-gray-100">
+        <div className="grid grid-cols-1 w-full">
+          <Droppable droppableId="one">
+            {(magic) => (
+              <div className="p-[30px] bg-gray-100 m-5 rounded-[5px] min-h-[200px]" ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((toDo, index) => (
+                  <Draggable key={toDo} draggableId={toDo} index={index}>
+                    {(magic) => (
+                      <div className="rounded-md mb-5 mt-5 p-[10px] bg-white-800 border border-black "
+                        ref={magic.innerRef}
+                        {...magic.dragHandleProps}
+                        {...magic.draggableProps}
+                      >
+                        {toDo}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </div>
+      </div>
+    </DragDropContext>
   );
 }
-
 export default App;
